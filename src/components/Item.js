e3f16a5f9517c54c4edef323daa69fc1aa77655b
Mercy <maritimmercy8@gmail.com>
2025-04-15 17:43:16 +0300

    added filter

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
   return (
-    <li className="">
+    <li className={inCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {!inCart && <button onClick={() => setInCart(true)} className="add">Add to Cart</button>}
+      {inCart && <button onClick={() => setInCart(false)} className="remove">Remove from Cart</button>}
     </li>
   );
 }
